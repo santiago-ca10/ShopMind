2e8c1531ed34a6ee9cee8abb64a1bbe6bf5b4bb8
SCA10 <santycardal2019@gmail.com>
2026-05-10 20:56:03 -0500

    feat: migrate frontend to React with Vite and connect product API to dynamic UI

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import { connectDB } from "./config/db.js";
 import productoRoutes from "./routes/producto.routes.js";
 import usuarioRoutes from "./routes/usuario.routes.js";
@@ -7,10 +9,11 @@ import usuarioRoutes from "./routes/usuario.routes.js";
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 connectDB();
-
 
 app.use("/api", productoRoutes);
 app.use("/api", usuarioRoutes);
